2e01b013c4256c3e9232cfe471049eae5dadf445
tinashekupfuwa <tinashekupfuwa@gmail.com>
2022-04-27 18:08:14 +0200

    bubblesort algorith

--- linear table definitions/day01.js
+++ linear table definitions/day01.js
@@ -3,4 +3,34 @@
 let scores = [40, 56, 22, 43, 10, 21, 42, 67];
 
 
- 
+for (let i = 0; i < scores.length; i++) { 
+  console.log(scores[i]+`,`);
+}
+
+
+
+/* Maximum of integer sequences-------------------------------------------------------------------------- */
+
+const maxArray = (maxinteger) => { 
+  for (let i = 0; i < maxArray.length; i++) { 
+    if (maxinteger[i] < maxinteger[i + 1]) { 
+      let temp = maxinteger[i + 1];
+      maxinteger[i] = maxinteger[i + 1];
+      maxValue = maxinteger[maxinteger.length-1];
+
+    }
+  }
+  return maxValue;
+}
+
+
+
+console.log(maxArray(scores));
+
+/* Bubble sort algorithm-------------------------------------------------------------------------- */
+
+const Bubblesort = (numArray) => { 
+  for (let i = 0; i < scores.length; i++) {
+    if(numArray[i]<numArray[])
+   }
+}
