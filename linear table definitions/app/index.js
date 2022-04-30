12308cf719fce54596271c886a640c9a1a3abf73
tinashekupfuwa <tinashekupfuwa@gmail.com>
2022-04-30 15:58:45 +0200

    few tweaks

---
+++ linear table definitions/app/index.js
@@ -0,0 +1,8 @@
+import { lineartable } from "./linear-table-definition";
+
+const ltable = document.querySelectorAll('.lineartable');
+
+let scores = [40, 56, 2, 87, 90, 32, 45, 123];
+ltable.textContent = 'this is working';
+
+console.log(`running`);
---
+++ linear table definitions/app/linear-table-definition.js
@@ -0,0 +1,15 @@
+//linear table is a sequence of elements.its a one-dimensional array.
+
+
+
+export const lineartable = (arrayInput) => { 
+  for (let i = 0; i < arrayInput.length; i++) { 
+    console.log(arrayInput[i] + ',');
+  }
+
+}
+
+
+lineartable(scores);
+
+export default lineartable();
