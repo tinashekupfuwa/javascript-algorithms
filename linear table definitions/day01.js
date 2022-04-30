2c1251ea0d2226db150572d94272f3b80da6a41d
tinashekupfuwa <tinashekupfuwa@gmail.com>
2022-04-30 11:53:07 +0200

    added a css file

--- linear table definitions/day01.js
+++ linear table definitions/day01.js
@@ -1,36 +1,13 @@
 //linear table is a sequence of elements.its a one-dimensional array.
 
-let scores = [40, 56, 22, 43, 10, 21, 42, 67];
+let scores = [40, 56, 2, 87, 90, 32, 45, 123];
 
-
-for (let i = 0; i < scores.length; i++) { 
-  console.log(scores[i]+`,`);
-}
-
-
+for (let i = 0; i < scores.length; i++) {
+  document.write(scores[i]+',');
+  console.log(scores[i] + ',');
+ }
 
 /* Maximum of integer sequences-------------------------------------------------------------------------- */
 
-const maxArray = (maxinteger) => { 
-  for (let i = 0; i < maxArray.length; i++) { 
-    if (maxinteger[i] < maxinteger[i + 1]) { 
-      let temp = maxinteger[i + 1];
-      maxinteger[i] = maxinteger[i + 1];
-      maxValue = maxinteger[maxinteger.length-1];
-
-    }
-  }
-  return maxValue;
-}
-
-
-
-console.log(maxArray(scores));
 
 /* Bubble sort algorithm-------------------------------------------------------------------------- */
-
-const Bubblesort = (numArray) => { 
-  for (let i = 0; i < scores.length; i++) {
-    if(numArray[i]<numArray[])
-   }
-}
---
+++ linear table definitions/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+for (let i = 0; i < scores.length; i++) {
+//   // document.write(scores[i]+',');
+//   console.log(scores[i] + ',');
+//  }
