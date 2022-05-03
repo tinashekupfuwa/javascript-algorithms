4c4b388ceb536a6963ce600065b59cce95b1fa8e
tinashekupfuwa <tinashekupfuwa@gmail.com>
2022-05-03 02:24:18 +0200

    tryouts with zsh

--- linear table definitions/app/MaximumValue.js
+++ linear table definitions/app/MaximumValue.js
@@ -1,6 +1,6 @@
 const maxValue = (randArray) => { 
   for (let i = 0; i < randArray.length-1;i++) { 
-    if (randArray[i] > randArray[i+1]) {
+    if (randArray[i] > randArray[i+1]) {  // this finds maximum value, changing the inequality gives the opposite.
       let temp = randArray[i];
       randArray[i] = randArray[i + 1];
       randArray[i + 1] = temp;
